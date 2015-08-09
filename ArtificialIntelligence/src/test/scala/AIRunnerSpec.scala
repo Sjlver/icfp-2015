16503e71a0150f1527825c01a59e823f14da663f
Jonas Wagner <jonas.wagner@epfl.ch>
2015-08-08 22:46:18 -0700

    Another unit test for the AIRunner

--- ArtificialIntelligence/src/test/scala/AIRunnerSpec.scala
+++ ArtificialIntelligence/src/test/scala/AIRunnerSpec.scala
@@ -1,6 +1,20 @@
 import scala.util.Random
 import spray.json._
 
+object AIRunnerSpec {
+  val TWOSEEDS_JSON = """
+    {
+      "height":1,
+      "width":1,
+      "sourceSeeds":[17, 42],
+      "units":[{"members":[{"x":0,"y":0}],"pivot":{"x":0,"y":0}}],
+      "id":42,
+      "filled":[],
+      "sourceLength":100
+    }
+  """ 
+}
+
 class AIRunnerSpec extends UnitSpec {
   "An AIRunner" should "run AIs" in {
     Random.setSeed(42)
@@ -25,5 +39,25 @@ class AIRunnerSpec extends UnitSpec {
     }
     
     // println(solutionsArray.compactPrint)
+  }
+  
+  "An AIRunner" should "process all seeds" in {
+    Random.setSeed(42)
+    val board = new Board
+    board.fromJson(AIRunnerSpec.TWOSEEDS_JSON)
+    board.sourceSeeds should be (Array(17, 42))
+    
+    val aiRunner = new AIRunner(
+        board,
+        b => new SamplingAI(b),
+        c => new PowerPhraseEncoder(c),
+        "AIRunnerSpec")
+    
+    val solutionsArray = aiRunner.run()
+    println(solutionsArray.prettyPrint)
+    solutionsArray match {
+      case JsArray(solutions) =>
+        solutions.size should be (2)
+    }
   }
 }
