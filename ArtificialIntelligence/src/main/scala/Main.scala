2a4dc0f2e81ff71dd955a0c91934e7f66f0802b6
Necto <necto.ne@gmail.com>
2015-08-09 07:01:38 +0200

    Main.scala merged

--- ArtificialIntelligence/src/main/scala/Main.scala
+++ ArtificialIntelligence/src/main/scala/Main.scala
@@ -1,11 +1,73 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    if (args.size < 2)
-      println("Please specify input file")
-    else {
-      val board = new Board()
-      board.fromJson(scala.io.Source.fromFile(args(1)).getLines.mkString)
+import java.nio.file.{Paths, Files}
+import java.nio.charset.StandardCharsets
 
+import java.io.File
+
+object SolveIt {
+
+  var inputFname = ""
+  var time = -1
+  var memory = -1
+  var cores = -1
+  var phrases :Seq[String] = Seq()
+  var outputFname = ""
+  var nMoves = -1
+  
+  def parseArgs(list: List[String]): Boolean = {
+      list match {
+        case Nil => true
+        case "-f" :: fname :: tail =>
+          if (inputFname != "")
+            println ("Hmm... got more than one input files. Whan should I do with it?")
+          inputFname = fname
+          parseArgs(tail)
+        case "-out" :: fname :: tail =>
+          outputFname = fname
+          parseArgs(tail)
+        case "-moves" :: n :: tail =>
+          nMoves = n.toInt
+          parseArgs(tail)
+        case "-t" :: t :: tail =>
+          if (time != -1)
+            println ("Hmm... got more than one time bounds")
+          time = t.toInt
+          parseArgs(tail)
+        case "-m" :: mem :: tail =>
+          if (memory != -1)
+            println ("Hmm... got more than one memory bounds")
+          memory = mem.toInt
+          parseArgs(tail)
+        case "-c" :: c :: tail =>
+          if (cores != -1)
+            println ("Hmm... got more than one core numbers")
+          cores = c.toInt
+          parseArgs(tail)
+        case "-p" :: phrase :: tail =>
+          phrases = phrases :+ phrase
+          parseArgs(tail)
+        case _ =>
+          println("Invalid option: " + list.toString)
+          return false
+      }
+    
+  } 
+  
+  def main(args: Array[String]): Unit = {
+    
+    if (args.length == 0) {
+     println("please supply some options")
+     return
+    }
+    val argList = args.toList
+    if (!parseArgs(argList))
+      println("Invalid options")
+    else {
+      if (inputFname == "") {
+        println("Must specify an input file!")
+        return
+      }
+      val board = new Board()
+      board.fromJson(scala.io.Source.fromFile(inputFname).getLines.reduceLeft(_+_))
       val aiRunner = new AIRunner(
         board,
         b => new SamplingAI(b),
@@ -15,5 +77,6 @@ object Main {
       val result = aiRunner.run().prettyPrint
       println(result)
     }
-  }
+  }/*   Files.write(Paths.get(args(2)), configs.getBytes(StandardCharsets.UTF_8));
+    */
 }
